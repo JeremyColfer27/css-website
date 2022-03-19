d9184449a3c69d0506bf6863efc0a07692e64feb
jeremycolfer27 <ii21038@bristol.ac.uk>
2022-03-19 01:07:07 +0000

    enriched jason virus featuring phoney buttons and clingy jas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Virus{
-    constructor(id){
+    constructor(id, closeButton){
         this.id = id;
     }
 
@@ -8,15 +8,60 @@ class Virus{
         document.getElementById('jason-virus-audio').play();
     }
 
+    hide(){
+        document.getElementById(this.id).style = "display: none;";
+    }
+
+
 }
+
 let jasonVirus = new Virus("virus1");
+let topLeftClose = document.getElementById("jason-virus-close-top-left");
+let topRightClose = document.getElementById("jason-virus-close-top-right");
+let botRightClose = document.getElementById("jason-virus-close-bot-right");
+let botLeftClose = document.getElementById("jason-virus-close-bot-left");
+
+
+topLeftClose.addEventListener("click", function()
+{
+    topLeftClose.style = "display:none;";
+    topRightClose.style = "display:block;";
+}
+);
+topRightClose.addEventListener("click", function()
+
+
+{
+    topRightClose.style = "display:none;";
+    botLeftClose.style = "display:block;";
+}
+);
+
+botLeftClose.addEventListener("click", function()
+
+
+{
+    botLeftClose.style = "display:none;";
+    botRightClose.style = "display:block;";
+}
+);
+
+botRightClose.addEventListener("click", function()
+{
+    jasonVirus.hide();
+}
+);
+
+
+
+
 
 
 setTimeout(function(){
     jasonVirus.show();
 
-}, 15000);
+}, 1000);
 
-showJasonVirus();
+//showJasonVirus();
 
 
